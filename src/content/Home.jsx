3021426426a0763b2729c757139603b7a4b4fc89
Favot <favot.corentin.b@gmail.com>
2021-02-15 23:50:30 +0100

    good for tonight

--- src/content/Home.jsx
+++ src/content/Home.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import BlogList from "./BlogsList";
+
 const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
@@ -11,14 +13,15 @@ const Home = () => {
       id: 3,
     },
   ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} handleDelete={handleDelete} />
     </div>
   );
 };
