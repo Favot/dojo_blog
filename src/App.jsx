8284ec06ab55422380ca2ff6af314699777c2a2c
Favot <favot.corentin.b@gmail.com>
2021-02-16 17:45:42 +0100

    a few episodes more

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import Navbar from "./navbar/Navbar";
 import Home from "./content/Home";
 
 function App() {
-  const title = "Welcome tot the new Blog";
-  const likes = 50;
   return (
     <div className="App">
       <Navbar />
--- src/content/Home.jsx
+++ src/content/Home.jsx
@@ -1,27 +1,23 @@
 import { useState } from "react";
+import useFetch from "../useFetch";
 
 import BlogList from "./BlogsList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const { data, isPending, error } = useFetch("http://localhost:8000/blogs");
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // };
+
+  // fetch data with useEffect a the first loading
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} handleDelete={handleDelete} />
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {data && <BlogList blogs={data} />}
     </div>
   );
 };
